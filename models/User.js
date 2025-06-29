6fd748ba116bbf134837779d1413979ca211201d
jadu82 <mailid2firebase@gmail.com>
2025-06-29 03:42:03 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,10 +4,11 @@ const userSchema = new mongoose.Schema({
     uniqueid: { type: String, required: true, unique: true },
     entries: [
         {
-            fullName: { type: String, required: true },
-            motherName: { type: String, required: true },
-              dateOfBirth: { type: String, required: true },
-            submittedAt: { type: Date, default: Date.now }
+            mobileNumber:   { type: String, required: true },
+            fullName:       { type: String, required: true },
+            motherName:     { type: String, required: true },
+            dateOfBirth:    { type: String, required: true },
+            submittedAt:    { type: Date,   default: Date.now }
         }
     ]
 });
